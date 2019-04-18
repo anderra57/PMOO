3a5edf6e8f087d104c6c449867485f6f08d423ae
ANDER <ANDER@LAPTOP-62VJ7F0K>
2019-04-18 18:25:33 +0200

    idegoera

--- proiektua/Banketxea.java
+++ proiektua/Banketxea.java
@@ -4,6 +4,7 @@ public class Banketxea extends Egoera{
 	
 	public Banketxea(String pDeskribapena, ListaAkzioak pLista){
 		super(pDeskribapena, pLista);
+		this.idEgoera=3;
 	}
 	public void eszenatokiaInprimatu(){
 		
