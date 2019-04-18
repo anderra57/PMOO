d0d7f7a3cd931f94d01b22fcb64628b8a8b1884d
ANDER <ANDER@LAPTOP-62VJ7F0K>
2019-04-18 18:14:13 +0200

    idegoera

--- proiektua/Egoera.java
+++ proiektua/Egoera.java
@@ -3,6 +3,7 @@ package packproiektua;
 public abstract class Egoera {
 	private String deskribapena;
 	private ListaAkzioak lista;
+	private int idEgoera;
 	
 	public Egoera(String pDeskribapena, ListaAkzioak pLista){
 		this.deskribapena = pDeskribapena;
