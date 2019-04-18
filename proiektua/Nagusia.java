173aa2b9a9933bb8f3cc6cb10906b2b456d2585a
ANDER <ANDER@LAPTOP-62VJ7F0K>
2019-04-18 19:15:34 +0200

    main

--- proiektua/Nagusia.java
+++ proiektua/Nagusia.java
@@ -2,7 +2,7 @@ package proiektua;
 
 public class Nagusia {
     public static void main(String[] args) {
-
+    	//matrix
         matrizea matrize = new matrizea();
         matrize.eszenatokiaInprimatu();
         
