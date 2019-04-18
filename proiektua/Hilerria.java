80e6e9be76803ad3b6db1e2c66b09ef65b424f53
ANDER <ANDER@LAPTOP-62VJ7F0K>
2019-04-18 18:25:46 +0200

    idegoera

--- proiektua/Hilerria.java
+++ proiektua/Hilerria.java
@@ -4,6 +4,7 @@ public class Hilerria extends Egoera{
 	
 	public Hilerria(String pDeskribapena, ListaAkzioak pLista){
 		super(pDeskribapena, pLista);
+		this.idEgoera=2;
 	}
 	public void eszenatokiaInprimatu(){
 		
--- proiektua/Saloia.java
+++ proiektua/Saloia.java
@@ -4,6 +4,7 @@ public class Saloia extends Egoera{
 	
 	public Saloia(String pDeskribapena, ListaAkzioak pLista){
 		super(pDeskribapena, pLista);
+		this.idEgoera=1;
 	}
 	public void eszenatokiaInprimatu(){
 		
