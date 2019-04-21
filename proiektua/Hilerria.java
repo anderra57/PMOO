96dd9e301896fa59e212795f3f72247734c73b3e
Iker <Iker@192.168.1.109>
2019-04-21 13:58:08 +0200

    hilerria print

--- proiektua/Hilerria.java
+++ proiektua/Hilerria.java
@@ -7,6 +7,24 @@ public class Hilerria extends Egoera{
 		this.idEgoera=2;
 	}
 	public void eszenatokiaInprimatu(){
-		//hola
+		for (int i=0;i<20;i++) {
+			  for (int j=0;j<20;j++) {
+				  if(i==0 || i==19){
+					  matrizea[i][j]='+';
+				  }
+				  else if(j==0 || j==19){
+					  matrizea[i][j]='+';
+				  }
+				  else{
+					  matrizea[i][j]='-';
+				  }
+			  }
+		}
+		for (int i=0;i<20;i++) {
+			  for (int j=0;j<20;j++) {
+				  System.out.print(matrizea[i][j]+" ");
+			  }
+			  System.out.println();
+		}
 	}
 }
