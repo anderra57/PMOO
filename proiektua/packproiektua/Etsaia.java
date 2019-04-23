ba79067a1c9dba18a3c1935c96756461ebb2b0c1
ANDER <ANDER@LAPTOP-62VJ7F0K>
2019-04-23 19:44:36 +0200

    etsaia

--- proiektua/packproiektua/Etsaia.java
+++ proiektua/packproiektua/Etsaia.java
@@ -2,12 +2,11 @@ package packproiektua;
 
 public class Etsaia {
 	
-	private int bizitza;
-	private int atq;
+	private int pv;
+	private static int atq=5;
 
-	public Etsaia(int pAtq){
-		this.atq = pAtq;
-		this.bizitza = 100;
+	public Etsaia(){
+		this.pv = 100;
 	}
 	
 	public void eraso(){
@@ -15,16 +14,17 @@ public class Etsaia {
 		//En este metodo, lo qeu haria seria, si nuestro personaje esta cubierto, el etsaia nos va a quitar 0 de vida.
 		//Si nuestro personaje no esta cubierto, que nos quite (5 o 10) de vida
 		if(Protagonista.nireProtagonista().estalita()){
+			emaitza = Protagonista.nireProtagonista().getPv();
 			Protagonista.nireProtagonista().setBizitzaBerria(emaitza);
 		}
 		else{
-			emaitza = Protagonista.nireProtagonista().getPv() - 10;
+			emaitza = Protagonista.nireProtagonista().getPv() - atq;
 			Protagonista.nireProtagonista().setBizitzaBerria(emaitza);
 		}
 	}
 	public boolean bizirik(){
 		boolean bizirikDago = false;
-		if(this.bizitza <= 0){
+		if(this.pv > 0){
 			bizirikDago = true;
 		}
 		return bizirikDago;
