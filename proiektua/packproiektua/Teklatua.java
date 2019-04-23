e247873a43622ff7a8bc9afe5d5c44ecbec5abc1
adeiarias <49227821+adeiarias@users.noreply.github.com>
2019-04-23 19:26:14 +0200

    Add files via upload

---
+++ proiektua/packproiektua/Teklatua.java
@@ -0,0 +1,26 @@
+package packproiektua;
+
+import java.util.Scanner;
+
+public class Teklatua {
+	
+	private Scanner sc;
+	private static Teklatua nireTeklatua = null;
+	
+	private Teklatua(){
+		this.sc = new Scanner(System.in);
+	}
+	
+	public static Teklatua getNireTeklatua(){
+		if(nireTeklatua == null){
+			nireTeklatua = new Teklatua();
+		}
+		return nireTeklatua;
+	}
+	
+	public int irakurriZenb() throws NumberFormatException {
+		String sar = this.sc.nextLine();
+		int zenb = Integer.parseInt(sar);
+		return zenb;
+	}
+}
