04b800e22361aa1bfa71e13a5808d28b9d503331
ANDER <ANDER@LAPTOP-62VJ7F0K>
2019-05-01 23:44:56 +0200

    mapaIrakurri sortuta

--- proiektua/packproiektua/FitxeroakIrakurri.java
+++ proiektua/packproiektua/FitxeroakIrakurri.java
@@ -13,7 +13,7 @@ public class FitxeroakIrakurri {
 		
 	}
 	
-	private static FitxeroakIrakurri getNireFItxeroakIrakurri(){
+	private static FitxeroakIrakurri getNireFitxeroakIrakurri(){
 		if(nireFitxeroakIrakurri==null){
 			nireFitxeroakIrakurri=new FitxeroakIrakurri();
 		}
@@ -27,6 +27,33 @@ public class FitxeroakIrakurri {
 		while((katea=b.readLine())!=null){
 			System.out.println(katea);
 		}
+		b.close();
+	}
+	
+	public static void mapaIrakurri(String pFitxeroa) throws FileNotFoundException, IOException{
+		char[][] matrizea = new char[20][20];
+		FileReader f=new FileReader(pFitxeroa);
+		BufferedReader b=new BufferedReader(f);
+				
 		
+		
+		for (int i=0;i<20;i++) {
+			  for (int j=0;j<20;j++) {
+				  char c = (char)b.read();
+				  matrizea[i][j]=c;
+						  
+			  }
+		}
+		
+		for (int i=0;i<20;i++) {
+			  for (int j=0;j<20;j++) {
+				  System.out.print(matrizea[i][j]+" ");
+			  }
+			  System.out.println();
+		}
+		
+		
+		b.close();
+		//return matrizea;
 	}
 }
