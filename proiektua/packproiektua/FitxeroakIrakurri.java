e6478080e040df30e94ea06f884ef184b41d2a04
ANDER <ANDER@LAPTOP-62VJ7F0K>
2019-05-04 02:09:00 +0200

    fitx2

--- proiektua/packproiektua/FitxeroakIrakurri.java
+++ proiektua/packproiektua/FitxeroakIrakurri.java
@@ -65,7 +65,6 @@ public class FitxeroakIrakurri {
 		
 		BufferedReader b=new BufferedReader(new FileReader("bin//fitxategiak//" + pFitxeroa));
 		
-		
 		for (int i=0;i<20;i++) {
 			  for (int j=0;j<20;j++) {
 				  char c = (char)b.read();
