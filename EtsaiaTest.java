17b7b7458e0ac6ac5a449d5faacda33c9ebd8ee2
jonan <jonan@LAPTOP-F4HNS0U7>
2019-05-27 14:53:01 +0200

    EtsaiaTest eginda mirar si se puede hacer bien lo del random de eraso

--- EtsaiaTest.java
+++ EtsaiaTest.java
@@ -7,53 +7,53 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class EtsaiaTest {
-
+	Etsaia e1,e2,e3;
+	
 	@Before
 	public void setUp() throws Exception {
+		e1=new Etsaia("Lehenengo Etsaia",10,10);
+		e2=new Etsaia("Bigarren Etsaia",11,11);
+		e3=new Etsaia("Hirugarren Etsaia",12,12);
+		Protagonista.hasieratuProtagonista("Protagonista proba");
 	}
 
 	@After
 	public void tearDown() throws Exception {
 	}
 
-	@Test
-	public void testGetX() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetY() {
-		fail("Not yet implemented");
-	}
 
 	@Test
 	public void testEtsaia() {
-		fail("Not yet implemented");
+		assertNotNull(e1);
+		assertNotNull(e2);
+		assertNotNull(e3);
 	}
-
-	@Test
-	public void testGetPv() {
-		fail("Not yet implemented");
-	}
-
+	
 	@Test
 	public void testEraso() {
-		fail("Not yet implemented");
+		
+		e1.eraso();
+		if(Protagonista.getNireProtagonista().estalita()){
+			assertEquals(Protagonista.getNireProtagonista().getPv(),250);
+		}
+		else {
+			assertEquals(Protagonista.getNireProtagonista().getPv(),240);
+		}
 	}
-
+	
 	@Test
 	public void testHilda() {
-		fail("Not yet implemented");
+		
+		assertFalse(e1.hilda());
+		e1.setBizitza(0);
+		assertTrue(e1.hilda());
+		
+		assertFalse(e2.hilda());
+		e2.setBizitza(0);
+		assertTrue(e2.hilda());
+		
+		assertFalse(e3.hilda());
+		e3.setBizitza(0);
+		assertTrue(e3.hilda());
 	}
-
-	@Test
-	public void testGetIzena() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetBizitza() {
-		fail("Not yet implemented");
-	}
-
 }
